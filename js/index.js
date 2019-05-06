b2e0931b746558d18177d87637a75512fbf2a5ed
Roger Lee <rmlee0003@gmail.com>
2019-05-05 21:13:06 -0400

    added select event and 2 more key events

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 const home = document.querySelector(".home");
 document.addEventListener('mousemove', event => {
  
- home.style.color = `rgb(55,${event.clientX/3},55)`;
+ home.style.color = `rgb(55,${event.clientX / 4},55)`;
 });
 
 const boat_background = document.querySelector(".content-destination");
@@ -38,6 +38,7 @@ fun_img.addEventListener('dblclick', event => {
 inv_holder = document.querySelector('.inverse-content');
 inverse_image = document.querySelector('.inverse-content img');
 let newContent = document.createElement("h2");
+
 inverse_image.addEventListener('load', event => {
  inverse_image.style.display = 'none';
  
@@ -49,20 +50,45 @@ inverse_image.addEventListener('load', event => {
 
 inc = 0;
 function wheelPos(event) {
-
+ event.preventDefault();
  if (event.deltaY < 0) {
  inc += 10;
  console.log(parseInt(inc));
- home.style.backgroundColor= `rgb(${inc},55,55)`;
+ home.style.backgroundColor= `rgb(${inc},${inc},${inc})`;
  }
  else {
   inc -= 10;
  console.log(parseInt(inc));
- home.style.backgroundColor= `rgb(${inc},55,55)`;
+ home.style.backgroundColor= `rgb(${inc},${inc},${inc})`;
  }
 };
-home.onwheel = wheelPos;
+window.onwheel = wheelPos;
 
 window.addEventListener('scroll', event => {
  newContent.textContent = "HEY, YOU SCROLLED!...don't do that.";
 });
+
+const text = document.createElement('textarea');
+text.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation';
+text.style.width = '100%';
+text.style.height = '80px';
+home.appendChild(text);
+function select(event) {
+ 
+ const selection = event.target.value.substring(event.target.selectionStart, event.target.selectionEnd);
+ newContent.textContent = `${selection}`;
+};
+text.addEventListener('select', select);
+
+const mid = document.querySelector('.content-section');
+
+document.addEventListener('keydown', event =>{
+ if(event.code == 'ArrowLeft'){
+  mid.style.color = 'white';
+ }
+ });
+ document.addEventListener('keyup', event =>{
+  if(event.code == 'ArrowLeft'){
+   mid.style.color = '';
+  }
+  });
